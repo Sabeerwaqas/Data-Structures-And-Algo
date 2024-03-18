3b1222da5770c9a95af79cca12eaf5415eb3522e
sabeer-mangotech <sabeer.mangotech@gmail.com>
2024-03-18 23:52:39 +0500

    [Add: Create a linked list]

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -1,5 +1,48 @@
 // Node class
 
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+
+// Linked list class
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.size = 0;
+//   }
+// }
+
+// Add data
+
+// add(data){
+//     const newNode = new Node(data);
+//     if(this.head === null){
+//         this.head = newNode;
+//     }
+//     else{
+//         let current = this.head;
+//         while(current.next !== null){
+//             current = current.next;
+//         }
+//         current.next = newNode;
+//     }
+//     this.size++;
+// }
+
+// Create linked list
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+}
+
+// Create node
+
 class Node {
   constructor(data) {
     this.data = data;
@@ -7,29 +50,16 @@ class Node {
   }
 }
 
-// Linked list class
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.size = 0;
-  }
-}
+let node1 = new Node(2);
+let node2 = new Node(5);
 
 
-// Add data
+node1.next = node2;
 
-add(data){
-    const newNode = new Node(data);
-    if(this.head === null){
-        this.head = newNode;
-    }
-    else{
-        let current = this.head;
-        while(current.next !== null){
-            current = current.next;
-        }
-        current.next = newNode;
-    }
-    this.size++;
-}
+// Create a linked list
+
+let list = new LinkedList(node1);
+
+console.log(list.head.data);
+
+
