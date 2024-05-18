05be8b05f16a4fe271eac667a16c2bd8987b3cfa
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-05-18 04:48:36 -0700

    [Explore Sets in JavaScript]

---
+++ sets/index.js
@@ -0,0 +1,9 @@
+let users = new Set();
+users.add("John");
+users.add("David");
+users.add("Kevin")
+
+console.log("Users exist =>",users);
+console.log("users size =>", users.size)
+
+console.log("Has visitor =>", users.has("John"))
