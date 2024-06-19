dbebb04b9251078639bcb8fa2f0f9abc93ab38ce
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-06-19 15:56:20 +0500

    Find another selection sort algorithm

--- algorithms/selectionSort/index.js
+++ algorithms/selectionSort/index.js
@@ -1,27 +1,37 @@
 function selectionSort(arr) {
-  let arrayLength = arr.length;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let minIndex = i;
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[j] < arr[minIndex]) {
+//         minIndex = j;
+//       }
+//     }
+//     if (minIndex !== i) {
+//       let temp = arr[minIndex];
+//       arr[minIndex] = arr[i];
+//       arr[i] = temp;
+//     }
+//   }
 
-  for (let i = 0; i < arrayLength - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arrayLength; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
+//   return arr;
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
       }
     }
-    if (minIndex !== i) {
-      let temp = arr[minIndex];
-      arr[minIndex] = arr[i];
-      arr[i] = temp;
+    if (i !== lowest) {
+      // swap the elements
+      [arr[i],arr[lowest]] = [arr[lowest], arr[i]];
     }
   }
 
   return arr;
 }
 
-
 let array = [7, 9, 3, 5, 15, 10, 0];
 
 console.log("Original Array =>", array);
 console.log("Sorted Array =>", selectionSort(array));
-
-
