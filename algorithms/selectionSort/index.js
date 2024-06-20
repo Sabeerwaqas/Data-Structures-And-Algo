c27406473b19e36bf121c88f29dbecd90c915be2
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-06-20 09:58:47 +0500

    Explore another selection sort approach

--- algorithms/selectionSort/index.js
+++ algorithms/selectionSort/index.js
@@ -1,19 +1,19 @@
 function selectionSort(arr) {
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     let minIndex = i;
-//     for (let j = i + 1; j < arr.length; j++) {
-//       if (arr[j] < arr[minIndex]) {
-//         minIndex = j;
-//       }
-//     }
-//     if (minIndex !== i) {
-//       let temp = arr[minIndex];
-//       arr[minIndex] = arr[i];
-//       arr[i] = temp;
-//     }
-//   }
+  //   for (let i = 0; i < arr.length - 1; i++) {
+  //     let minIndex = i;
+  //     for (let j = i + 1; j < arr.length; j++) {
+  //       if (arr[j] < arr[minIndex]) {
+  //         minIndex = j;
+  //       }
+  //     }
+  //     if (minIndex !== i) {
+  //       let temp = arr[minIndex];
+  //       arr[minIndex] = arr[i];
+  //       arr[i] = temp;
+  //     }
+  //   }
 
-//   return arr;
+  //   return arr;
 
   for (let i = 0; i < arr.length; i++) {
     let lowest = i;
@@ -23,15 +23,14 @@ function selectionSort(arr) {
       }
     }
     if (i !== lowest) {
-      // swap the elements
-      [arr[i],arr[lowest]] = [arr[lowest], arr[i]];
+      [arr[i], arr[lowest]] = [arr[lowest], arr[i]];
     }
   }
 
   return arr;
 }
 
-let array = [7, 9, 3, 5, 15, 10, 0];
+let array = [3,2,1,3];
 
 console.log("Original Array =>", array);
 console.log("Sorted Array =>", selectionSort(array));
