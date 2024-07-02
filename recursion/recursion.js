d059d7d541da13a978176be52fec9a38665fa74a
Sabeerwaqas <sabeerwaqas050@gmail.com>
2024-07-02 21:40:01 +0500

    chore: practice backtracking recursion

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -1,8 +1,17 @@
-function recursion(i, n) {
-    if (i > n) {
+function recursion(i, j) {
+    if (i > j) {
         return;
     }
-    console.log(i);
-    recursion(i + 1, n);
+    console.log("Normal case", i);
+    recursion(i + 1, j);
 }
 recursion(1, 10);
+// print number using backtracking
+function recursiveFunction(k, l) {
+    if (k > l) {
+        return;
+    }
+    recursiveFunction(k + 1, l);
+    console.log("Backtracking =>", k);
+}
+recursiveFunction(1, 10);
--- recursion/recursion.ts
+++ recursion/recursion.ts
@@ -1,9 +1,21 @@
-function recursion(i: number, n: number) {
-  if (i > n) {
+function recursion(i: number, j: number) {
+  if (i > j) {
     return;
   }
-  console.log(i);
-  recursion(i + 1, n);
+  console.log("Normal case", i);
+  recursion(i + 1, j);
 }
 
 recursion(1, 10);
+
+// print number using backtracking
+
+function recursiveFunction(k: number, l: number) {
+  if (k > l) {
+    return;
+  }
+  recursiveFunction(k + 1, l);
+  console.log("Backtracking =>", k);
+}
+
+recursiveFunction(1, 10);
