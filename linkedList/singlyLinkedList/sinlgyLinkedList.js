ea00d4ba2ff2577774cdb81f821b24313ddac10c
Sabeerwaqas <sabeerwaqas050@gmail.com>
2025-01-05 23:24:40 +0500

    Chore: Learn to write 'appendNode', 'inserAt', 'removeFrom' and 'printList' in linkedlist.

--- linkedList/singlyLinkedList/sinlgyLinkedList.js
+++ linkedList/singlyLinkedList/sinlgyLinkedList.js
@@ -1,47 +1,61 @@
-// Node class
-
-// class Node {
-//   constructor(data) {
-//     this.data = data;
-//     this.next = null;
-//   }
-// }
-
-// Linked list class
-
-// class LinkedList {
-//   constructor() {
-//     this.head = null;
-//     this.size = 0;
-//   }
-// }
-
-// Add data
-
-// add(data){
-//     const newNode = new Node(data);
-//     if(this.head === null){
-//         this.head = newNode;
-//     }
-//     else{
-//         let current = this.head;
-//         while(current.next !== null){
-//             current = current.next;
-//         }
-//         current.next = newNode;
-//     }
-//     this.size++;
-// }
-
-// Create linked list
-
 class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
-}
 
-// Create node
+  appendNode(newNode) {
+    let node = this.head;
+    if (node == null) {
+      this.head = newNode;
+      return;
+    }
+    while (node.next) {
+      node = node.next;
+    }
+    node.next = newNode;
+  }
+
+  insertAt(index, newNode) {
+    let node = this.head;
+    if (index == 0) {
+      newNode.next = node;
+      this.head = newNode;
+      return;
+    }
+    while (--index) {
+      if (node.next !== null) node = node.next;
+      else throw Error("Index Out of Bound");
+    }
+    let tempVal = node.next;
+    node.next = newNode;
+    newNode.next = tempVal;
+  }
+
+  removeFrom(index) {
+    let node = this.head;
+    if (index === 0) {
+      if (node !== null) {
+        node = node.next;
+        this.head = node;
+      } else throw Error("Index Out of Bound");
+
+      return;
+    }
+    while (--index) {
+      if (node.next !== null) node = node.next;
+      else throw Error("Index Out of Bound");
+    }
+    node.next = node.next.next;
+  }
+
+  printList() {
+    let node = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
+  }
+}
 
 class Node {
   constructor(data) {
@@ -50,16 +64,16 @@ class Node {
   }
 }
 
-let node1 = new Node(2);
-let node2 = new Node(5);
+let myList = new LinkedList();
+let initialNode = new Node(5);
 
+myList.appendNode(initialNode);
+myList.appendNode(new Node(6));
+myList.appendNode(new Node(11));
+myList.appendNode(new Node(7));
 
-node1.next = node2;
+// Insert a node at index 2 (between 6 and 11)
+myList.insertAt(2, new Node(9));
+myList.removeFrom(4);
 
-// Create a linked list
-
-let list = new LinkedList(node1);
-
-console.log(list.head.data);
-
-
+myList.printList();
