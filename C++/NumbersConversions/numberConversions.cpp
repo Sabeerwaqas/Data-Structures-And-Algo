6c436eaa818bedd505afa3669c4566f02e891ebe
Sabeerwaqas <sabeerwaqas050@gmail.com>
2025-03-08 19:52:18 +0500

    Chore: Write a function to convert 'Decimal To Binary'

---
+++ C++/NumbersConversions/numberConversions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// Decimal To Binary
+
+string decimalToBinary(int num)
+{
+
+    int givenNum = num;
+    string bin = "";
+    while (givenNum > 0)
+    {
+        int lastDigit = givenNum % 2;
+        bin.push_back('0' + lastDigit);
+        lastDigit /= 2;
+    }
+    reverse(bin.begin(), bin.end());
+    return bin;
+}
+
+int main()
+{
+
+    // -----> Decimal To Binary <----- \\
+
+    cout << "Decimal To Binary: " << decimalToBinary(2453) << endl;
+
+    return 0;
+}
